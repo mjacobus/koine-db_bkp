2267340109c4a8d3bef8dc5a2bbebbc918258be2
Marcelo Jacobus <marcelo.jacobus@xing.com>
2017-09-21 23:49:47 +0200

    Add coveralls report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,20 @@
 require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  [
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+)
 SimpleCov.start
 
-require "bundler/setup"
-require "koine/db_bkp"
+require 'bundler/setup'
+require 'koine/db_bkp'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
